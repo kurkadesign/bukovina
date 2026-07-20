39584a06cceac7f8c90c6bdd39fde1c346654a34
kurkadesign <124005589+kurkadesign@users.noreply.github.com>
2026-07-20 14:04:12 +0200

    Update state.js

--- js/state.js
+++ js/state.js
@@ -5,13 +5,13 @@ export const TYPES=[
   ['round-table','Okrúhly stôl','fa_circle',140,140,8,{icon:false,text:true,shape:'circle'}],
   ['rect-table','Obdĺžnikový stôl','fa_table_cells_large',180,100,8,{icon:false,text:true,shape:'rounded'}],
   ['head-table','Hlavný svadobný stôl','fa_rings_wedding',260,90,8,{icon:false,text:false,shape:'rounded'}],
-  ['chair','Stolička','fa_chair',55,55,0,{icon:true,text:true,shape:'chair'}],
+  ['chair','Stolička','fa_seat',55,55,0,{icon:true,text:true,shape:'chair'}],
   ['dj','DJ pult','fa_turntable',150,80,0,{icon:true,text:true,shape:'rounded'}],
   ['speaker','Reproduktor','fa_speaker',55,55,0,{icon:true,text:false,shape:'rounded'}],
   ['bar','Bar','fa_martini_glass_citrus',170,70,0,{icon:true,text:true,shape:'rounded'}],
-  ['dance-floor','Tanečný parket','fa_border_all',250,200,0,{icon:true,text:true,shape:'sharp'}],
-  ['decoration','Dekorácia','fa_wand_magic_sparkles',70,70,0,{icon:true,text:true,shape:'none'}],
-  ['plant','Rastlina','fa_seedling',70,70,0,{icon:true,text:true,shape:'none'}],
+  ['dance-floor','Tanečný parket','fa_shoe_prints',250,200,0,{icon:true,text:true,shape:'sharp'}],
+  ['decoration','Dekorácia','fa_balloon',70,70,0,{icon:true,text:true,shape:'none'}],
+  ['plant','Rastlina','fa_flower_tulip',70,70,0,{icon:true,text:true,shape:'none'}],
   ['stage','Pódium','fa_masks_theater',220,110,0,{icon:true,text:true,shape:'rounded'}],
   ['photo','Fotokútik','fa_camera_retro',180,120,0,{icon:true,text:true,shape:'rounded'}]
 ];
