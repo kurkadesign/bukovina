eed437c33c57d828263b3abe58b44f6df3bf5842
kurkadesign <kurkadesign@users.noreply.github.com>
2026-07-22 14:04:32 +0200

    Use stable asset versions for faster loading

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 $html = file_get_contents(__DIR__ . '/index.html');
-$version=(string)random_int(10000,99999);
+$assetFiles=['style.css','panel.css','readonly.css','css/fontawesome.css','css/sharp-light.css','js/bootstrap.js','js/app.js','js/state.js'];
+$version=(string)max(array_map(static fn(string $file):int=>(int)@filemtime(__DIR__.'/'.$file),$assetFiles));
 $html = str_replace('</head>', '<link rel="stylesheet" href="readonly.css"></head>', $html);
 $html = preg_replace('/<script type="module" src="js\/app\.js(?:\?v=[^"]*)?"><\/script>/', '<script type="module" src="js/bootstrap.js"></script>', $html);
 $html = preg_replace('/((?:href|src)="(?!https?:\/\/)[^"?]+\.(?:css|js))(?:\?v=[^"]*)?"/', '$1?v='.$version.'"', $html);
